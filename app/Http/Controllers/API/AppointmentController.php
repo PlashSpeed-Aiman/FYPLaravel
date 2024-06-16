5c28adc52152d0165d18b0d9dea762abe85f98eb
aiman <maiman.ar@live.iium.edu.my>
2024-06-17 00:53:02 +0800

    Updates

--- app/Http/Controllers/API/AppointmentController.php
+++ app/Http/Controllers/API/AppointmentController.php
@@ -3,26 +3,33 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Client\Request;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class AppointmentController extends Controller
 {
-//    private Request $request;
-//    private Auth $auth;
-//    public function __construct(Request $request, Auth $auth)
-//    {
-//        $this->request = $request;
-//        $this->auth = $auth;
-//    }
+    private Request $request;
+    private Auth $auth;
+    public function __construct(Request $request, Auth $auth)
+    {
+        $this->request = $request;
+        $this->auth = $auth;
+    }
     public function store()
     {
-        $client = \App\Models\Client::find(1);
+        $user = Auth::user();
+        $data = $this->request->validate([
+            'date' => 'required|date',
+            'time' => 'required',
+            'reason' => 'required'
+        ]);
+        $client = \App\Models\Client::find($user->id);
+
         $appointment = new \App\Models\Appointment();
         $appointment->appointment_number = 'APPT-'.rand(1000,9999);
-        $appointment->date = '2021-01-01';
-        $appointment->time = '10:00:00';
-        $appointment->reason = 'Checkup';
+        $appointment->date = $data['date'];
+        $appointment->time = $data['time'];
+        $appointment->reason = $data['reason'];
         $appointment->appointment_status = 'pending';
         $client->appointments()->save($appointment);
         return $appointment;
--- resources/views/client/appointment/request.blade.php
+++ resources/views/client/appointment/request.blade.php
@@ -9,20 +9,21 @@
             </div>
             <h1 class="text-xl font-bold mx-10">Appointment Booking</h1>
             <p class="my-3 mx-10">Book your appointments and have them reviewed by the admin</p>
-            <form class="my-5 w-full flex flex-col gap-2">
+            <form method="post" action="{{route('client.appointment.store')}}" class="my-5 w-full flex flex-col gap-2">
+                @csrf {{ csrf_field() }}
                 <h1 class="text-xl font-bold mx-12">Date</h1>
-                <input type="date" class="input border border-zinc-200 mx-10">
+                <input name="date" type="date" class="input border border-zinc-200 mx-10">
                 <h1 class="text-xl font-bold mx-12">Time</h1>
-                <input type="time" class="input border border-zinc-200 mx-10">
+                <input name="time" type="time" class="input border border-zinc-200 mx-10">
                 <h1 class="text-xl font-bold mx-12">Reason</h1>
-                <textarea class="input border border-zinc-200 mx-10"></textarea>
+                <textarea name="reason" class="input border border-zinc-200 mx-10"></textarea>
                 <input type="submit" class="btn btn-ghost bg-blue-900  ml-auto mr-10" value="Request">
             </form>
 
 
-
         </div>
     </section>
+
 @endsection
 
 
