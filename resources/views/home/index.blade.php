ceb0a25416e2294fc8e89f82497f37cf5553c952
aiman <maiman.ar@live.iium.edu.my>
2024-06-19 16:58:42 +0800

    Updates

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,7 +4,7 @@
     <div id="navbar" class="my-5">
         <ul class="flex gap-1 justify-between">
             <li><a href="/" class="text-3xl font-[Poppins]">Zarkasya & Co.</a></li>
-            <div class="flex gap-3 justify-center items-center">
+            <section class="flex gap-3 justify-center items-center">
                 <li class="font-bold"><a href="{{ url('/') }}">Home</a></li>
                 @if(!Auth::check())
                 <li class="font-bold"><a href="{{ url('/login') }}">Login</a></li>
@@ -13,11 +13,14 @@
                 <li class="font-bold"><a href="{{ url('/logout') }}">Logout</a></li>
 
                 @endif
-            </div>
+            </section>
            </ul>
     </div>
     <hr>
     <div id="content" class="h-64">
+        <h1 class="text-3xl font-bold">Welcome to Zarkasya & Co.</h1>
+        <p class="my-5">Zarkasya & Co. is a company that provides services in the field of accounting and tax consulting. We are here to help you manage your finances and taxes. We have a team of experts who are ready to help you. We are committed to providing the best service to our clients. We are here to help you manage your finances and taxes. We have a team of experts who are ready to help you. We are committed to providing the best service to our clients.</p>
+        <a href="{{ url('/login') }}" class="btn">Get Started</a>
 
     </div>
 </section>
